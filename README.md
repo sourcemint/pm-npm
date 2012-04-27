04214d69aa45fe1880f1f241a4f938220fa30948
cadorn <christoph@christophdorn.com>
2012-04-27 14:29:36 -0700

    mappings package cleanup

--- README.md
+++ README.md
@@ -8,9 +8,3 @@ Sourcemint Package Manager plugin for NPM Packages
   * Docs License: [Creative Commons Attribution-NonCommercial-ShareAlike 3.0](http://creativecommons.org/licenses/by-nc-sa/3.0/)
   * Sponsor: [Sourcemint](http://sourcemint.com/)
   * Mailing list: [groups.google.com/group/sourcemint](http://groups.google.com/group/sourcemint)
-
-
-TODO
-====
-
-  * 'postprograminstall' script - https://github.com/isaacs/npm/issues/2354
--- npm-packages/mappings/scripts/postinstall-notice.js
+++ npm-packages/mappings/scripts/postinstall-notice.js
@@ -1,9 +1,6 @@
 
-const TERM = require("sourcemint-util-js/lib/term");
-
-
 if (process.env.SM_CLI_CALL !== "true") {
-    TERM.stdout.writenl("\0red(" + "------------------------------------------------------------------------------------------------------------" + "\0)");
-    TERM.stdout.writenl("\0red(" + "|\0bold(  Use the `sm` command from `npm install -g sm` to `sm [install|update|link|...] .` this package/program. \0)|" + "\0)");
-    TERM.stdout.writenl("\0red(" + "------------------------------------------------------------------------------------------------------------" + "\0)");
+    process.stdout.write("------------------------------------------------------------------------------------------------------------" + "\n");
+    process.stdout.write("|  Use the `sm` command from `npm install -g sm` to `sm [install|update|link|...] .` this package/program. |" + "\n");
+    process.stdout.write("------------------------------------------------------------------------------------------------------------" + "\n");
 }
